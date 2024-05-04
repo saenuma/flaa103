f7b51626d2aa84af2b69853560e781bd037e22ed
Bankole Ojo <ojobankole@gmail.com>
2024-05-04 22:34:28 +0100

    began the launch two servers linux command

---
+++ lib.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func GetRootPath() (string, error) {
+	hd, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.Wrap(err, "os error")
+	}
+
+	dd := os.Getenv("SNAP_USER_COMMON")
+
+	if strings.HasPrefix(dd, filepath.Join(hd, "snap", "go")) || dd == "" {
+		dd = filepath.Join(hd, "flaa103")
+		os.MkdirAll(dd, 0777)
+	}
+
+	return dd, nil
+}
+
+func DoesPathExists(p string) bool {
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func UntestedRandomString(length int) string {
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
