ff1f39e9d173e31d7f731fd2132c352f3f9cc304
Bankole Ojo <ojobankole@gmail.com>
2024-05-09 11:21:04 +0100

    did a quick resize function

---
+++ gcasrn/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/saenuma/flaa103"
+	"github.com/saenuma/zazabul"
+)
+
+func main() {
+	inputPath := "/var/snap/flaarum/common/input.zconf"
+	conf, err := zazabul.LoadConfigFile(inputPath)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+
+	flaa103.ConfObject = make(map[string]string)
+
+	flaa103.ConfObject["project"] = conf.Get("project")
+	flaa103.ConfObject["zone"] = conf.Get("zone")
+	flaa103.ConfObject["instance"] = conf.Get("instance")
+	flaa103.ConfObject["timezone"] = conf.Get("timezone")
+	flaa103.ConfObject["machine-type-day"] = conf.Get("machine-type-day")
+	flaa103.ConfObject["machine-type-night"] = conf.Get("machine-type-night")
+
+	flaa103.ResizeToDayMachineType()
+}
